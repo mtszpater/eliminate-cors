be62dcc4add4609ff33e078c2ac75428b8bf3046
Mateusz Pater <mtszpater@gmail.com>
2020-02-08 16:00:45 +0100

    add example controller

---
+++ server/src/main/java/dev/pater/cors/Controller.java
@@ -0,0 +1,13 @@
+package dev.pater.cors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world!";
+    }
+}
